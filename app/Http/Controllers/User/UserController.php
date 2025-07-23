ce295e474c09aac84e21eb159557c059a60357ca
Ali AlSaghir <alisalsaghir@gmail.com>
2025-07-23 23:23:48 +0300

    separate user validation into its own class

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,28 +3,18 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\CapsuleResource;
 use App\Services\UserService;
-use App\Traits\ResponseTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller {
-  use ResponseTrait;
 
-  public function update(Request $request) {
+  public function update(UpdateUserRequest $request) {
     $user = Auth::guard('api')->user();
 
-    $request->validate([
-      'name' => 'sometimes|string|max:255',
-      'profile_picture' => 'sometimes|string',
-    ]);
-
-    if ($request->has('email') || $request->has('password')) {
-      return $this->responseJSON('Email and password cannot be updated', 'error', 403);
-    }
-
-    $updatedUser = UserService::updateUser($user, $request->all());
+    $updatedUser = UserService::updateUser($user, $request->validated());
 
     return $this->responseJSON([
       'message' => 'User updated successfully',
